427dadf6a20453cb3faead35f5c803cbb813f9d1
Arnau <agomez1f@gmail.com>
2020-10-21 15:11:15 +0200

    🗂 Set up folder and file structure

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <div>
+        {/*Here goes the logo*/}
+        <h1>Notorious</h1>
+        <p>idea generator</p>
+        {/*Here go the login links, action buttons, etc.*/}
+      </div>
+    </nav>
+  )
+}
+
+export default Header
